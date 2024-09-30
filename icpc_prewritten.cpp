32bae8796d6da21c1faddd7ab96b8ac085352f20
ba40k <mad_rad@bk.ru>
2024-09-30 20:28:34 +0300

    Update icpc_prewritten.cpp
    added dsu

--- icpc_prewritten.cpp
+++ icpc_prewritten.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 namespace icpc
 {
     class SqrtDec // пустой шаблон
@@ -31,11 +31,46 @@ namespace icpc
         }
         
     };
+    class Dsu // базовая дсу 
+    {
+        private:
+            std::vector<int> parent;
+            std::vector<int> len;
+            void build (int n)
+            {
+                parent.resize(n);
+                len.resize(n,1);
+                for (int i =0;i<n;++i) 
+                {
+                    parent[i] = i;
+                }
+            }
+        public:
+            Dsu(int n)
+            {
+                build(n);
+            }
+            int get_parent(int vertex)
+            {
+                return parent[vertex] = parent[vertex]==vertex?vertex:get_parent(parent[vertex]);
+            }
+            void set_union(int a, int b)
+            {
+                a = get_parent(a);
+                b = get_parent(b);
+                len[a]<len[b]?std::swap(a,b):std::swap(a,a);
+                len[a] += len[b];
+                parent[b] = a;
+            }
+    };
  // алгосы тут 
 }
 
 int main()
 {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
     //обращение к алгосам  icpc::
     
 }
