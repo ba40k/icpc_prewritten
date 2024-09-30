eb11edca57b5f853565e6c44464aab0d449c0ecd
ba40k <mad_rad@bk.ru>
2024-09-30 19:57:40 +0300

    added sqrt_dec class

--- icpc_prewritten.cpp
+++ icpc_prewritten.cpp
@@ -1,11 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 namespace icpc
 {
+    class SqrtDec // пустой шаблон
+    {
+    private : 
+        const int k = 8;
+        int block_size = 1 << k; // default 
+
+        struct node
+        {
+           
+        };
+        std::vector<node>  dec;
+        std::vector<int> vec;
+        void build(std::vector<int>& input)
+        {
+            vec = input;
+            dec.resize((input.size()>>k) + 1);
+            for (int i = 0; i < input.size(); ++i)
+            {
+                // creating
+            }
+        }
+    public:
+        SqrtDec(std::vector<int>& input)
+        {
+            build(input);
+        }
+        
+    };
  // алгосы тут 
 }
 
 int main()
 {
     //обращение к алгосам  icpc::
+    
 }
