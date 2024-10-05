d453ee10cf774836d183ae61dbac1cf14c92039b
Nikita Lukyanovich <papilionoidea4@gmail.com>
2024-10-05 19:05:44 +0300

    Add suffix automaton

--- icpc_prewritten.cpp
+++ icpc_prewritten.cpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <limits>
 #include <cstring>
+#include <map>
 
 using namespace std;
 
@@ -567,7 +568,59 @@ void sufmas(string s, int n)
         memcpy(c, cn, n * sizeof(int));
     }
 }
+namespace SuffixAutomaton
+{
+struct state
+{
+    int len, link;
+    std::map<char, int> next;
+};
 
+constexpr int MAXLEN = 100000;
+
+state st[MAXLEN * 2];
+
+int sz, last;
+
+void sa_init()
+{
+    sz = last = 0;
+    st[0].len = 0;
+    st[0].link = -1;
+    ++sz;
+    /*
+
+    for (int i=0; i<MAXLEN*2; ++i)
+        st[i].next.clear();
+    */
+}
+
+void sa_extend(char c)
+{
+    int cur = sz++;
+    st[cur].len = st[last].len + 1;
+    int p;
+    for (p = last; p != -1 && !st[p].next.count(c); p = st[p].link)
+        st[p].next[c] = cur;
+    if (p == -1)
+        st[cur].link = 0;
+    else {
+        int q = st[p].next[c];
+        if (st[p].len + 1 == st[q].len)
+            st[cur].link = q;
+        else {
+            int clone = sz++;
+            st[clone].len = st[p].len + 1;
+            st[clone].next = st[q].next;
+            st[clone].link = st[q].link;
+            for (; p != -1 && st[p].next[c] == q; p = st[p].link)
+                st[p].next[c] = clone;
+            st[q].link = st[cur].link = clone;
+        }
+    }
+    last = cur;
+}
+}
 }
 
 int main()
